e88d22352d6c42d5bf409146e502e953fd24168a
Arman7052 <99arman7052@gmail.com>
2023-03-31 07:18:37 +0600

    Page Layout done

--- src/assets/component/Blogs/Blogs.jsx
+++ src/assets/component/Blogs/Blogs.jsx
@@ -9,7 +9,7 @@ const Blogs = () => {
             .then(data => setBlog(data))
     }, [])
     return (
-        <div className='grid grid-cols-3  gap-4'>
+        <div className='grid grid-cols-1 lg:grid-cols-3  gap-4'>
             <div className='col-span-2 '>
                 {
                     blog.map((card) => <Card
@@ -18,7 +18,14 @@ const Blogs = () => {
                     ></Card>)
                 }
             </div>
-            <div className=''>a</div>
+            <div className='pt-4'>
+                <div className=' border h-20 rounded-lg bg-indigo-300	pt-3'>
+                    <strong className=' font-semibold text-3xl'>Spent time on read :</strong>
+                </div>
+                <div className='text-start  border rounded-lg mt-4'>
+                    <h1 className=' font-bold text-xl ps-2'>Bookmarked Blogs :</h1>
+                </div>
+            </div>
         </div>
     );
 };
